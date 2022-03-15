6b38d9352b2a568d3830f3471a41175672915e04
AisyahMutmainnah <73178425+AisyahMutmainnah@users.noreply.github.com>
2022-03-16 00:27:36 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Hello from "./components/Hello";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      
+      <Footer />
+      
+      
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
